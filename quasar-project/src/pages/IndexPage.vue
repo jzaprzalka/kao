2c153438de3f26d878b363693ba82fa8ea6dc117
Anita Stasiełuk <ans99@onet.pl>
2022-10-13 22:02:14 +0200

    Started work on choosing language view

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,17 +1,52 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      alt="KAO logo"
+      src="~assets/logoKAO.png"
+      style="width: 200px; height: 200px; margin-top: 10em;"
     >
+
+    <hr
+      style="flex-basis: 100%; height: 0; margin: 0; border: 0;"
+    />
+
+    <q-select
+        rounded outlined bottom-slots
+        v-model="model"
+        :options="options"
+        label="Choose language"
+        :dense="dense"
+        :options-dense="denseOpts"
+        style="width: 250px;">
+      <template v-slot:before>
+        <q-icon name="language" />
+      </template>
+    </q-select>
+
+    <hr
+      style="flex-basis: 100%; height: 0; margin: 0; border: 0;"
+    />
+
+    <q-btn outline rounded color="primary" size="lg" label="GET STARTED"/>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  setup () {
+    return {
+      model: ref(null),
+      options: [
+        'Polish', 'English', '<3', 'Apple', 'Oracle', '🤮🤮🤮🤮🤮🤮'
+      ],
+
+      dense: ref(false),
+      denseOpts: ref(false)
+    }
+  }
 })
 </script>
