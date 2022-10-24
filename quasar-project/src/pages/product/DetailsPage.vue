f7181dc571689b98a2c8943d5d9c794c45794556
Anita Stasiełuk <a.stasieluk99@gmail.com>
2022-10-24 17:57:55 +0200

    displaying full image and actual product name

--- quasar-project/src/pages/product/DetailsPage.vue
+++ quasar-project/src/pages/product/DetailsPage.vue
@@ -1,17 +1,25 @@
 <template>
   <q-page class="flex-center">
     <q-img
-      src="~assets/logoKAO.png"
+      :src="barcode.image_front_small_url"
       style="width: 100%;
       position: fixed;
       top: 0;
       left: 0;"
       :fit="'cover'"
-    />
-    <div class="q-pa-md" style="padding-top: 45%;">
+      :ratio="16/9"
+      @click="showImage = true"
+    >
+      <template v-slot:error>
+        <div class="absolute-full flex flex-center bg-negative text-white">
+          Cannot load image
+        </div>
+      </template>
+    </q-img>
+    <div class="q-pa-md" style="padding-top: 40%;">
       <q-card bordered style="border-radius: 30px;">
         <q-card-section>
-          <div class="text-h5">Product Name</div>
+          <div class="text-h5"> {{barcode.product_name}}  </div>
         </q-card-section>
       </q-card>
     </div>
@@ -78,19 +86,51 @@
       </q-card>
     </div>
 
+    <q-dialog
+      v-model="showImage"
+    >
+      <q-card style="width: 100%">
+        <q-card-section>
+          <div class="text-h6">Full product image</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-img
+            :src="barcode.image_front_small_url"
+            style="width: 100%;"
+            :fit="'cover'"
+            @click="showImage = true"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="OK" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
   </q-page>
 </template>
 
 <script>
 
+import {useRouter} from "vue-router";
+import {ref} from "vue";
+
 export default {
   name: "DetailsPage",
 
-
-  mounted() {
-    let _this = this
-    let barcode = JSON.parse(_this.$router.currentRoute.value.query.barcode)
+  setup() {
+    const router = useRouter()
+    let barcode = JSON.parse(router.currentRoute.value.query.barcode)
     console.log(barcode)
+
+    return {
+      barcode,
+      showImage: ref(false)
+    }
+  },
+  mounted() {
   }
 }
 </script>
